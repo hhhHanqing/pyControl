17d71f4756a574ea772d8a72edd7f0a4705f4483
andy_lustig <alustig3@gmail.com>
2020-04-22 11:42:16 -0400

    commit from room computer

--- config/markov_electric.py
+++ config/markov_electric.py
@@ -8,12 +8,12 @@ board = Breakout_dseries_1_6()
 button      = _h.Digital_input(board.button,rising_event='button_release',falling_event='button',pull='up')
 
 ########## Top Row #############
-Llever      = Lever_electric(board.port_4, lever_event = 'L_lever')
-Rlever      = Lever_electric(board.port_3, lever_event = 'R_lever')
-houselight  = _h.Digital_output(board.port_6.POW_A)
 Lpoke       = Nosepoke(board.port_1, nose_event = 'L_nose', lick_event = 'L_lick' )
 Cpoke       = Nosepoke(board.port_2, nose_event = 'C_nose')
+Rlever      = Lever_electric(board.port_3, lever_event = 'R_lever')
+Llever      = Lever_electric(board.port_4, lever_event = 'L_lever')
 Rpoke       = Nosepoke(board.port_5, nose_event = 'R_nose', lick_event = 'R_lick' )
+houselight  = _h.Digital_output(board.port_6.POW_A)
 
 ######### Bottom Row ###########
 # empty port 7
