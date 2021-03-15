62a350adb77e29094652dfc7e94cd3aca28d1bf0
pyControl_Computer <karpova.lab@gmail.com>
2021-03-15 17:55:35 -0400

    cleand data is now saved directly in animal folders on DM11/data/behavior/raw. No longer saved to separate pyControlData sub folder. Also removed unused code for created html tables and interactive graphs for markov data

--- tools/data_cleaner.py
+++ tools/data_cleaner.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 from config.paths import dirs
 
-cleaner_version = 2021031000 ## YearMonthDayRevision YYYYMMDDrr  can have up to 100 revisions/day
+cleaner_version = 2021031500 ## YearMonthDayRevision YYYYMMDDrr  can have up to 100 revisions/day
 
 class Log_cleaner():
     def __init__(self,file_path):
@@ -21,37 +21,23 @@ class Log_cleaner():
 
         self.print_data = pd.DataFrame(session.print_lines[i+1:])
 
-        if self.session.task_name == 'sequence':
-            self.data_folder_path = os.path.join(dirs['network_dir'],"Sequence_Training")
-        else:
-            self.data_folder_path = os.path.join(dirs['network_dir'],"Markov_Training")
+        self.data_folder_path = dirs['network_dir']
 
     def clean(self):
         self.create_folders()
         self.create_dataframes(self.session.task_name)
         self.expand_results(self.session.task_name)
-        # self.create_html_table()
-        # self.create_markov_bokeh()
         self.save_json()
         self.move_raw_txtfile()
 
     def create_folders(self):
         rat = str(self.session.subject_ID)
         try:
-            os.mkdir(os.path.join(self.data_folder_path,rat))
-            print('made new directory')
+            new_dir = os.path.join(self.data_folder_path,rat)
+            os.mkdir(new_dir)
+            print(F'Animal directory not found, therefore creating new directory {new_dir}')
         except:
             pass
-
-        folder_names = ['raw','cleaned_json']
-
-        for fn in folder_names:
-            try:
-                newFolder = os.path.join(self.data_folder_path,rat,fn)
-                os.mkdir(newFolder)
-                print('Created {}'.format(newFolder))
-            except:
-                pass # folder already exists
 
     def create_dataframes(self,task):
         space_split = self.print_data[0].str.split(expand=True)
@@ -187,177 +173,13 @@ class Log_cleaner():
             event_dictionary[event_type] = list(self.session.times[event_type])
         json_dictionary['Events'] = event_dictionary
 
-        saveName = os.path.join(self.data_folder_path,str(self.session.subject_ID),"cleaned_json",self.session_name+".json")
+        saveName = os.path.join(self.data_folder_path,str(self.session.subject_ID),self.session_name+".json")
         with open(saveName,'w') as f:
             json.dump(json_dictionary,f,cls=NpEncoder)
 
-        print('json saved')
+        print(F'json saved at {saveName}\n')
 
     def move_raw_txtfile(self):
         import shutil
-        text_in_raw_folder = os.path.join(self.data_folder_path,str(self.session.subject_ID),"raw",self.session_name+".txt")
+        text_in_raw_folder = os.path.join(self.data_folder_path,str(self.session.subject_ID),self.session_name+".txt")
         shutil.move(self.txt_file,text_in_raw_folder)
-
-    def create_html_table(self):
-        pd.set_option('colheader_justify', 'center')   # FOR TABLE <th>
-
-        css_string = '''
-        .mystyle {
-            font-size: 11pt;
-            font-family: Arial;
-            border-collapse: collapse;
-            border: 1px solid silver;
-        }
-
-        .mystyle td, th {
-            padding: 5px;
-        }
-
-        th{
-            background: cornflowerblue;
-            color: white;
-            position: -webkit-sticky;
-            position: sticky;
-            top: 0;
-            z-index: 1;
-        }
-        
-        .mystyle tr:nth-child(even) {
-            background: #E0E0E0;
-        }
-
-        .mystyle tr:hover {
-            background: silver;
-            cursor: pointer;
-        }
-        '''
-
-        html_string = '''
-        <html>
-        <head><title>Raw Dataframe</title>
-        <style>
-        {css}
-        </style>
-        </head>  
-        
-        <body>
-            {table}
-        </body>
-        </html>.
-        '''
-
-        # OUTPUT AN HTML FILE
-        saveName = os.path.join(self.data_folder_path,self.session.subject_ID,"tables",f'{self.session_name}_table.html')
-        with open(saveName, 'w') as f:
-            f.write(html_string.format(css=css_string,table=self.combined.to_html(index=False,classes='mystyle')))
-
-    def create_markov_bokeh(self):
-        def getSeries(colName,rename):
-            series = self.combined.loc[self.combined[colName]==1,colName]
-            series[:] = rename
-            return series
-
-        L_Rwd = getSeries('Left_rewarded','Left')
-        L_NRwd = getSeries('Left_unrewarded','Left')
-        L_Rej = getSeries('Left_rejected','Left')
-        L_Err = getSeries('Left_error','Left')
-
-        R_Rwd = getSeries('Right_rewarded','Right')
-        R_NRwd = getSeries('Right_unrewarded','Right')
-        R_Rej = getSeries('Right_rejected','Right')
-        R_Err = getSeries('Right_error','Right')
-
-        import bokeh.plotting as bk
-        from bokeh.models import Legend,RangeTool
-        from bokeh.layouts import column
-
-        background_color = 'white'
-        correct_color = "green"
-        error_color = "red"
-        reject_color = "blue"
-
-        figWidth = 1250
-
-        p = bk.figure(
-            width = figWidth,
-            height=200,
-            tools="xwheel_zoom,xpan,reset,save",
-            active_scroll = "xwheel_zoom",
-            title='Rat {} ---- {} trials ---- {}'.format(self.session.subject_ID,len(self.combined),self.session.datetime_string),
-            y_range = ['Right','Left'],
-            x_range = [0,80],
-            background_fill_color = background_color,
-            x_axis_location="below",
-            toolbar_location="above",
-        )
-
-        p.ygrid.visible = False
-        p.xgrid.visible = True
-        markerSize = 10
-        left_full = p.circle(L_Rwd.index,L_Rwd,size=markerSize,line_color=correct_color,fill_color=correct_color)
-        left_empty = p.circle(L_NRwd.index,L_NRwd,size=markerSize,line_color=correct_color,fill_color=background_color)
-        left_cross = p.cross(L_Rej.index,L_Rej,size=markerSize,line_color=reject_color,line_width=2)
-        left_square = p.square(L_Err.index,L_Err,size=markerSize,line_color=error_color,fill_color=error_color)
-
-        right_full = p.circle(R_Rwd.index,R_Rwd,size=markerSize,line_color=correct_color,fill_color=correct_color)
-        right_empty = p.circle(R_NRwd.index,R_NRwd,size=markerSize,line_color=correct_color,fill_color=background_color)
-        right_cross = p.cross(R_Rej.index,R_Rej,size=markerSize,line_color=reject_color,line_width=2)
-        right_square = p.square(R_Err.index,R_Err,size=markerSize,line_color=error_color,fill_color=error_color)
-
-        outcome_legend = Legend(items=[
-            ("Reward",   [left_full]),
-            ("No Reward", [left_empty]),
-            ("Reject",   [left_cross]),
-            ("Error", [left_square]),    
-        ], location="center")
-
-        p.add_layout(outcome_legend, 'left')
-        p.legend.title="Choice Outcome"
-        p.legend.title_text_font_style = "bold"
-        ####################################################################################################
-        probabilities = bk.figure(plot_height=150,
-                        plot_width=figWidth,
-                        y_range=[0,1],
-                        x_range=p.x_range,
-                        tools="",
-                        toolbar_location=None,
-                        background_fill_color="white",
-                        y_axis_label='Reward Probability',
-                        y_minor_ticks=None,
-                        x_axis_location=None
-        )
-        right_line = probabilities.line(self.combined['Right_prob'].index,self.combined['Right_prob'],line_width=5,alpha=.5)
-        left_line = probabilities.line(self.combined['Left_prob'].index,self.combined['Left_prob'],color='orange',line_width=5,alpha=.5)
-
-        legend = Legend(items=[
-            ("Left",   [left_line]),
-            ("Right", [right_line]),
-        ], location="center")
-
-        probabilities.add_layout(legend, 'left')
-        probabilities.legend.title="Side"
-        probabilities.legend.title_text_font_style = "bold"
-        ####################################################################################################
-        select = bk.figure(plot_height=100,
-                        plot_width=figWidth,
-                        y_range=[0,1],
-                        tools="",
-                        toolbar_location=None,
-                        background_fill_color="white",
-                        x_axis_label='Trial',
-                        y_axis_location=None
-        )
-
-        range_tool = RangeTool(x_range=p.x_range)
-        range_tool.overlay.fill_color = "yellow"
-        range_tool.overlay.fill_alpha = 0.3
-
-        right_line = select.line(self.combined['Right_prob'].index,self.combined['Right_prob'],line_width=2,alpha=.5)
-        left_line = select.line(self.combined['Left_prob'].index,self.combined['Left_prob'],color='orange',line_width=2,alpha=.5)
-
-        select.add_tools(range_tool)
-        select.toolbar.active_multi = range_tool
-
-        bokeh_output_path = os.path.join(self.data_folder_path,self.session.subject_ID,"bokeh_plots",f'{self.session_name}_plots.html')
-        bk.output_file(bokeh_output_path)
-        bk.save(column(p,probabilities,select))
